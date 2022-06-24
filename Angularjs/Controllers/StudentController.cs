8c5495947798c01b5500d7860ae453d3c55ae3dd
vidhyalakhmi <vidhyavelu09@gmail.com>
2022-06-24 15:13:37 +0530

    angular

---
+++ Angularjs/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using Angularjs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angularjs.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Student()
+        {
+            return View();
+        }
+        public string InsertStudentRecord(Student std)
+        {
+            using(StuEntities db =new StuEntities())
+            {
+                db.Students.Add(std);
+                db.SaveChanges();
+                return "Student Added SuccessFully....!";
+            }
+        }
+        public JsonResult GetAllStudent()
+        {
+            using (StuEntities db =new StuEntities())
+            {
+                var AllRecord =db.Students.ToList();
+                return Json(AllRecord ,JsonRequestBehavior.AllowGet);
+            }
+        }
+        public string UpdateStudentRecord(Student std)
+        {
+            using (StuEntities db =new StuEntities())
+            {
+                var record=db.Students.Where(s => s.Id == std.Id).FirstOrDefault();
+                record.Name = std.Name;
+                record.Age = std.Age;
+                record.Department = std.Department;
+                db.SaveChanges();
+                return "Updated SuccessFully...!";
+            }
+        }
+        public string DeleteStudent(Student std) 
+        {
+            using (StuEntities db = new StuEntities())
+            {
+                var data =db.Students.Where(s => s.Id == std.Id).FirstOrDefault();
+                db.Students.Remove(data);
+                db.SaveChanges();
+                return "Delete SucessFully..!";
+            }
+
+        }
+    }
+}
---
+++ Angularjs/Scripts/Student.js
@@ -0,0 +1,79 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCntrl", function ($scope, $http) {
+    
+    $scope.GetAllStudent = function () {
+        $http({
+            method: "get",
+            url: "https://localhost:44376/Student/GetAllStudent"
+        }).then(function (response) {
+            $scope.Student = response.data;
+        }, function () {
+            alert("Error Occured");
+        })
+    };
+    $scope.InsertStudent = function () {
+        var type = document.getElementById("insertStd").getAttribute("value");
+        if (type == "Submit") {
+            $scope.Student = {};
+            $scope.Student.Name = $scope.SName;
+            $scope.Student.Age = $scope.SAge;
+            $scope.Student.Department = $scope.SDepartment;
+            $http({
+                method: "post",
+                url: "https://localhost:44376/Student/InsertStudentRecord",
+                datatype: "json",
+                data: JSON.stringify($scope.Student)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllStudent();
+                $scope.SName = "";
+                $scope.SAge = "";
+                $scope.SDepartment = "";
+
+            })
+
+        } else {
+            $scope.Student = {};
+            $scope.Student.Id = sessionStorage.getItem("SId");
+            $scope.Student.Name = $scope.SName;
+            $scope.Student.Age = $scope.SAge;
+            $scope.Student.Department = $scope.SDepartment;
+            $http({
+                method: "post",
+                url: "https://localhost:44376/Student/UpdateStudentRecord",
+                datatype: "json",
+                data: JSON.stringify($scope.Student)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllStudent();
+                $scope.SName = "";
+                $scope.SAge = "";
+                $scope.SDepartment = "";
+                document.getElementById("insertStd").setAttribute("value", "Submit");
+
+            })
+
+        }
+    };
+    $scope.UpdateStudent = function (Std) {
+        sessionStorage.setItem("SId", Std.Id);
+        $scope.SName = Std.Name;
+        $scope.SAge = Std.Age;
+        $scope.SDepartment = Std.Department;
+        document.getElementById("insertStd").setAttribute("value", "Update");
+
+    };
+    $scope.DeleteStudent = function (Std) {
+
+        $http({
+            method: "post",
+            url: "https://localhost:44376/Student/DeleteStudent",
+            datatype: "json",
+            data: JSON.stringify(Std)
+        }).then(function (response) {
+            alert("Delete SuccessFully..!");
+            $scope.GetAllStudent();
+        })
+    };
+
+});
